48f6cd7bb7453fc07eaaf8f617fc4be79076ecbe
Gleb Liutsko <gleb290303@gmail.com>
2020-09-07 19:59:43 +0400

    +ver

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def requirements():
 
 setup(
     name='uaviak_timetable',
-    version='1.6.1',
+    version='1.6.2',
     author='Gleb Liutsko',
     author_email='gleb290303@gmail.com',
     license='MIT',
